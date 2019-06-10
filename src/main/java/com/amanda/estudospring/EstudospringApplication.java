66df90f4f649b666a8a08795aa7ecf4351087ac9
Amanda Aguirre <amandamorenoaguirre@gmail.com>
2019-06-10 15:50:50 -0300

    O projeto foi criado

---
+++ src/main/java/com/amanda/estudospring/EstudospringApplication.java
@@ -0,0 +1,13 @@
+package com.amanda.estudospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanda/estudospring/EstudospringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amanda.estudospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstudospringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
