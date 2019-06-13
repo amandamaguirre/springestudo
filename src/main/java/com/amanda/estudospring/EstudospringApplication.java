eb9a5cf4f372f502cbf9494b8cb2c9776eb897c8
Amanda Aguirre <amandamorenoaguirre@gmail.com>
2019-06-13 11:32:07 -0300

    Operação de instanciação

--- src/main/java/com/amanda/estudospring/EstudospringApplication.java
+++ src/main/java/com/amanda/estudospring/EstudospringApplication.java
@@ -1,13 +1,33 @@
 package com.amanda.estudospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudospringApplication {
+import com.amanda.estudospring.domain.Categoria;
+import com.amanda.estudospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudospringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudospringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
